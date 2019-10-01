5173c8f43cfb8ef93b5fb884630fe0bf703e7692
der_nabe <34216014+Nabe0416@users.noreply.github.com>
2019-10-01 11:42:51 +0200

    Copied entire project

---
+++ GDY2P1Group1/Assets/Scripts/EnemyAI/EnemyAI.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    private GameObject tilemap;
+    private GameObject pc;
+
+    [SerializeField]
+    private ViewRange vr;
+    [SerializeField]
+    private HearRange hr;
+
+    public bool pcIsInView = false;
+
+    private Transform viewPos;
+    private Transform hearPos;
+
+    [SerializeField]
+    private Transform lastSeenPos;
+
+    [SerializeField]
+    private int Hitpoint;
+    private int HitpointMax = 10;
+
+    private void Start()
+    {
+        #region Refs.
+        pc = FindObjectOfType<CharacterMovement>().gameObject;
+        tilemap = FindObjectOfType<Tilemap>().gameObject;
+        #endregion
+        Hitpoint = HitpointMax;
+    }
+
+    private void Update()
+    {
+        if(viewPos != null)
+        {
+            pcIsInView = true;
+        }
+        else
+        {
+            pcIsInView = false;
+        }
+
+        CheckHP();
+    }
+
+    public void SeePlayerAt(Transform pos)
+    {
+        if(hearPos) Destroy(hearPos.gameObject);
+        viewPos = pos;
+        if (pcIsInView) ChasePlayerMode();
+        GetComponent<AIDestinationSetter>().target = viewPos;
+
+    }
+
+    public void HearSmtAt(Transform pos)
+    {
+        hearPos = pos;
+        SearchMode();
+        if (!pcIsInView) GetComponent<AIDestinationSetter>().target = hearPos;
+    }
+
+    public void ShootPlayer()
+    {
+
+    }
+
+    private void ChasePlayerMode()
+    {
+        GetComponent<AIPath>().maxSpeed = 3;
+    }
+
+    private void SearchMode()
+    {
+        GetComponent<AIPath>().maxSpeed = 1;
+    }
+
+    public void DamageHP(int value)
+    {
+        Hitpoint -= value;
+    }
+
+    private void CheckHP()
+    {
+        if(Hitpoint <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        print(this.gameObject + " is dead.");
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ GDY2P1Group1/Assets/Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //SerializeField can visualize the variable below in the inspector
+    [SerializeField]
+    private float MoveSpeed = 5;
+
+    [SerializeField]
+    private GameObject characterSprite;
+
+    private Inventory inv;
+
+    private Rigidbody2D rb;
+
+    //The direction that the character is facing.
+    private Vector2 dir;
+
+    [SerializeField]
+    private bool TestOnly;
+
+    private void Start()
+    {
+        #region Refs.
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        inv = FindObjectOfType<Inventory>();
+        #endregion
+        AdjustRb();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    #region Set Rigidbody properties
+    private void AdjustRb()
+    {
+        rb.gravityScale = 0;
+        rb.freezeRotation = true;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+    }
+    #endregion
+
+    #region Movement
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rb.MovePosition((Vector2)this.transform.position + new Vector2(x, y) * MoveSpeed * Time.deltaTime);
+        
+    }
+    #endregion
+
+    private Vector2 GetMousePosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void Update()
+    {
+        dir = (GetMousePosition() - (Vector2)transform.position).normalized;
+        characterSprite.transform.up = dir;
+        if(Input.GetMouseButtonDown(0))
+        {
+            UseItem();
+        }
+    }
+
+    private void UseItem()
+    {
+        inv.UseItem();
+    }
+
+    public Transform GetPlayerTransform()
+    {
+        return transform;
+    }
+
+    public Vector2 GetPCMouseDir()
+    {
+        return (GetMousePosition() - (Vector2)transform.position).normalized;
+    }
+}
