66d94ef1b82dee9e2eb2456242e7dfba1e4341df
der_nabe <34216014+Nabe0416@users.noreply.github.com>
2019-10-27 20:23:24 +0100

    See Description
    - Added player death, zombie mode.
    - Added UI for inventory
    - Added a end point and restart manager, but without art asset. The player will respawn (scene refresh) after death.
    - Fixed the issue that the missing enemy object remains in the light system array.

---
+++ GDY2P1Group1/Assets/Scripts/Misc/EndPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndPoint : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            GameRestart();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameEnd();
+    }
+
+    [ContextMenu("EndGame")]
+    void GameEnd()
+    {
+        print("Game End.");
+        SceneManager.LoadScene(0);
+    }
+
+    void GameRestart()
+    {
+        GameEnd();
+    }
+}
